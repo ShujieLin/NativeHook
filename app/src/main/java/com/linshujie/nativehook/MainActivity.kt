b030fa841a5766f3299a405ca96b706094a1e6f6
LinShuJie(林树杰/深圳) <linshujie@paxsz.local>
2024-03-15 18:05:57 +0800

    Initial commit

---
+++ app/src/main/java/com/linshujie/nativehook/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.linshujie.nativehook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.linshujie.nativehook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'nativehook' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'nativehook' library on application startup.
+        init {
+            System.loadLibrary("nativehook")
+        }
+    }
+}
